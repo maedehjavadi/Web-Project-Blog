df4ec58ca9f243309b05a1c97105a73fb410f08d
mobina-cloude <mobinaa.alihamzeh@gmail.com>
2020-01-03 18:26:16 +0330

    the editblog page updated

--- resources/views/posts/editblog.blade.php
+++ resources/views/posts/editblog.blade.php
@@ -14,7 +14,7 @@
           </span>
         @enderror
         
-        <input id="text"   type="text" class="form-control inp text-right @error('text') is-invalid @enderror" name="text" placeholder="متن خاطره" style="font-family:B Mehr;font-weight:bold;" value="{{ old('text') ?? $post->text }}"  autocomplete="text" autofocus>
+        <input id="text"   type="text" class="form-control input text-right @error('text') is-invalid @enderror" style="height:300px;" name="text" placeholder="متن خاطره" style="font-family:B Mehr;font-weight:bold;" value="{{ old('text') ?? $post->text }}"  autocomplete="text" autofocus>
         @error('name')
           <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
@@ -25,9 +25,9 @@
             <button class="btn btn-primary mr-3" style="font-family:B Mehr; font-weight:bold;">ثبت</button>  
         </div>
       </form>
-      <div>
+      <!-- <div>
       <button class="btn btn-secondary" style="font-family:B Mehr; font-weight:bold;">بازگشت</button>
-      </div>
+      </div> -->
     </div>
 </div>
 
